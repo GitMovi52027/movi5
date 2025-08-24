a947a403afec0dfdd73dc57a490c6a74ecce8a76
GitMovi52027 <francisco@movi5.co>
2025-08-24 11:26:50 -0500

    Delete movi5-main directory

--- movi5-main/prisma/migrations/20250703223001_add_trip_type/migration.sql
+++
@@ -1,8 +0,0 @@
--- Agregar el tipo ENUM triptype
-CREATE TYPE "TripType" AS ENUM ('BUS', 'BUS_BOAT', 'BOAT');
-
--- Agregar la columna tripType a la tabla Route
-ALTER TABLE "Route" ADD COLUMN     "tripType" "TripType" NOT NULL DEFAULT 'BUS';
-
--- Eliminar el valor por defecto después de la migración
-ALTER TABLE "Route" ALTER COLUMN "tripType" DROP DEFAULT; 
